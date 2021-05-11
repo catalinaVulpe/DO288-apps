50282ee717c06d7560a38ca6098d91174c2d98c8
catalinaVulpe <75664855+catalinaVulpe@users.noreply.github.com>
2021-05-11 15:52:43 +0300

    Update server.js

--- openshift-day8-2/server.js
+++ openshift-day8-2/server.js
@@ -7,6 +7,7 @@ const app = express()
 const port = 3000
 app.get('/', (req, res) =>{
 var con = mysql.createConnection({
+ host: "mysql72.day8.svc",
  user: process.env.default,
  password: process.env.default ,
  database: process.env.db
